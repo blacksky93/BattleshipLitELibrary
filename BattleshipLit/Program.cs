aebe10999926ea7784d16bc6bde322a565846bbb
mgapp <mgapp@MIKEGYO>
2025-06-02 17:36:01 -0400

    Battleship lite application

--- BattleshipLit/Program.cs
+++ BattleshipLit/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BattleshipLiteLibrary.Models;
+using BattleshipLitLibrary;
+// if building Console App, usually will need to reference libraries in Console, not reference console in library
 
 namespace BattleshipLite
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            WelcomeMessage();
+
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
 
             Console.ReadLine();
         }
@@ -18,6 +25,49 @@ namespace BattleshipLite
         {
             Console.WriteLine("Welcome to Battleship Lite");
             Console.WriteLine("Created By Michael Gappa");
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
+
+            // Ask the user for their name
+            output.UserName = AskForUsersName();
+            // Load up the shot grid
+            GameLogic.InitializeGrid(output);
+
+            // Ask the user for their 5 ship placements
+            PlaceShips(output);
+
+            // Clear the screen
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number { model.ShipLocations.Count + 1}: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again.");
+                }
+            } while (model.ShipLocations.Count < 5);
         }
     }
 }
---
+++ BattleshipLitLibrary/GameLogic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleshipLiteLibrary.Models;
+
+namespace BattleshipLitLibrary
+{
+    // since we're not storing data in a class, can be reused in other places
+    public static class GameLogic
+    {
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int> 
+            {  
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel();
+            spot.SpotLetter = letter;
+            spot.SpotNumber = number;
+            spot.Status = BattleshipLiteLibrary.Enums.GridSpotStatus.Empty;
+
+            model.ShotGrid.Add(spot);
+        }
+    }
+}
